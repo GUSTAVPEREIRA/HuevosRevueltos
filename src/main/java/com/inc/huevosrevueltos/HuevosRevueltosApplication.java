4371780b5ff8db274c330d363cc4b7bdb6e69d75
Pereira <gugupereira123@hotmail.com>
2024-12-26 08:56:21 -0300

    Started project

---
+++ src/main/java/com/inc/huevosrevueltos/HuevosRevueltosApplication.java
@@ -0,0 +1,13 @@
+package com.inc.huevosrevueltos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuevosRevueltosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuevosRevueltosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inc/huevosrevueltos/HuevosRevueltosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inc.huevosrevueltos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuevosRevueltosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
